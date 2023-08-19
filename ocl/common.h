97ea5702c240b3eb0512686698560dfaa9813843
ds2chc <ds2chc@gmail.com>
2023-08-19 16:36:11 +0900

    Add retaining for sharing CL objects

--- ocl/common.h
+++ ocl/common.h
@@ -30,6 +30,10 @@ namespace ocl {
 
   template <typename T>
   struct RawObjectHandler {
+    static void Retain(T type) {
+      
+    }
+    
     static void Release(T type) {
 
     }
@@ -42,12 +46,16 @@ namespace ocl {
     ObjectBase() : object_(nullptr) {};
 
     /// @brief Constructor based on the regluar OpenCL data type
-    ObjectBase(const _Object obj) : object_(obj) {}
-
-    ObjectBase(const ObjectBase& rhs) : object_(rhs.object_) {
-
+    ObjectBase(const _Object obj) : object_(obj) {
+      Retain();
     }
 
+    /// @brief Copy constructor
+    ObjectBase(const ObjectBase& rhs) : object_(rhs.object_) {
+      rhs.Retain();
+    }
+
+    /// @brief Move constructor
     ObjectBase(ObjectBase&& rhs) : object_(std::move(rhs.object_)) {
       
     }
@@ -61,6 +69,7 @@ namespace ocl {
       if (this->object_ != rhs.object_) {
         Release();
         this->object_ = rhs.object_;
+        rhs.Retain();
       }
       return *this;
     }
@@ -70,6 +79,7 @@ namespace ocl {
       if (this->object_ != rhs.object_) {
         Release();
         this->object_ = rhs.object_;
+        rhs.Retain();
       }
       return *this;
     }
@@ -77,9 +87,14 @@ namespace ocl {
     /// @brief Get raw object type
     const _Object& operator() () const { return object_; }
 
+    _Object& operator() () { return object_; }
 
   protected:
     _Object object_;
+
+    void Retain() const {
+      RawObjectHandler<_Object>::Retain(object_);
+    }
 
     void Release() {
       if (object_ != nullptr) {
@@ -92,33 +107,52 @@ namespace ocl {
 
 
   template <> struct RawObjectHandler<cl_device_id> {
+    static void Retain(cl_device_id device) {
+      CL_CHECK_ERROR(clRetainDevice(device));
+    }
+    
     static void Release(cl_device_id device) {
       CL_CHECK_ERROR(clReleaseDevice(device));
     } 
   };
 
   template <> struct RawObjectHandler<cl_context> {
+    static void Retain(cl_context context) {
+      CL_CHECK_ERROR(clRetainContext(context));
+    }
+
     static void Release(cl_context context) {
       CL_CHECK_ERROR(clReleaseContext(context));
     } 
   };
   
   template <> struct RawObjectHandler<cl_program> {
+    static void Retain(cl_program program) {
+      CL_CHECK_ERROR(clRetainProgram(program));
+    }
+
     static void Release(cl_program program) {
       CL_CHECK_ERROR(clReleaseProgram(program));
     }
   };
 
   template <> struct RawObjectHandler<cl_kernel> {
+    static void Retain(cl_kernel kernel) {
+      CL_CHECK_ERROR(clRetainKernel(kernel));
+    }
+
     static void Release(cl_kernel kernel) {
       CL_CHECK_ERROR(clReleaseKernel(kernel));
     }
   };
 
   template <> struct RawObjectHandler<cl_mem> {
+    static void Retain(cl_mem mem) {
+      CL_CHECK_ERROR(clRetainMemObject(mem));
+    }
+
     static void Release(cl_mem mem) {
       CL_CHECK_ERROR(clReleaseMemObject(mem));
     }
   };
-
 } // namespace cl
